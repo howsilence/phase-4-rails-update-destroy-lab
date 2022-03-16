bdfea7ed8615c2dba2b5ae94764ccce00a129825
howsilence <statton0104@gmail.com>
2022-03-15 21:43:57 -0400

    commit tree b1cdea607f6e767b952b7f9faa44b027e7e71df2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :plants, only: [:index, :show, :create]
+  # resources :plants, only: [:index, :show, :create]
+  #adding a destroy and patch so now we need all RESTful paths
+  resources :plants
 end
